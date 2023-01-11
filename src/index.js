28b03648aae690089330e988b6774a8de37ed461
ClaytonDewey <clay@dryadmedia.com>
2023-01-11 16:01:25 -0600

    Bookmark where to pick back up later

--- src/index.js
+++ src/index.js
@@ -11,3 +11,9 @@ app.use('/api/v1/workouts', v1WorkoutRouter);
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
 });
+
+/* 
+https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/
+
+If we try to add a new workout but forget to provide the "mode" property in our request body, we should see the error message along with the 400 HTTP error code.
+ */
