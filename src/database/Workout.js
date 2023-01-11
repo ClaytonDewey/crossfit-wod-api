200083f7ff1152f076df5f40c2e401393fb87c5c
ClaytonDewey <clay@dryadmedia.com>
2023-01-11 15:01:26 -0600

    Fix typo in filepath

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,5 +1,5 @@
 const DB = require('./db.json');
-const { saveToDatabase } = require('/utils');
+const { saveToDatabase } = require('./utils');
 
 const getAllWorkouts = () => {
   return DB.workouts;
