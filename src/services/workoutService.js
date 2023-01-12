e8af3b2ddcc261c8134378d943dd34020c57aac0
ClaytonDewey <clay@dryadmedia.com>
2023-01-11 19:06:50 -0600

    Add try/catch to createWorkout method

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -13,11 +13,15 @@ const createNewWorkout = (newWorkout) => {
   const workoutToInsert = {
     ...newWorkout,
     id: uuid(),
-    createdAt: new Date().toLocaleDateString('en-US', { timeZone: 'UTC' }),
-    updatedAt: new Date().toLocaleDateString('en-US', { timeZone: 'UTC' }),
+    createdAt: new Date().toLocaleString('en-US', { timeZone: 'UTC' }),
+    updatedAt: new Date().toLocaleString('en-US', { timeZone: 'UTC' }),
   };
-  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
-  return createdWorkout;
+  try {
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+    return createdWorkout;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const updateOneWorkout = (workoutId, changes) => {
