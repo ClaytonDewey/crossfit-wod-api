cf473456d443605333015698e7139a806f6cca09
ClaytonDewey <clay@dryadmedia.com>
2023-01-12 14:54:19 -0600

    Add deleteOneMember controller

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -87,9 +87,30 @@ const updateOneMember = (req, res) => {
   }
 };
 
+const deleteOneMember = (req, res) => {
+  const {
+    params: { memberId },
+  } = req;
+  if (!memberId) {
+    res.status(400).send({
+      status: 'FAILED',
+      data: { error: "Parameter ':memberId' can not be empty" },
+    });
+  }
+  try {
+    memberService.deleteOneMember(memberId);
+    res.status(204).send({ status: 'OK' });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: 'FAILED', data: { error: error?.message || error } });
+  }
+};
+
 module.exports = {
   getAllMembers,
   getOneMember,
   createNewMember,
   updateOneMember,
+  deleteOneMember,
 };
