6595da5016ff42d50afea099647b983b8739f32e
ClaytonDewey <clay@dryadmedia.com>
2023-01-12 12:16:10 -0600

    Create controller for adding new member.

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -31,4 +31,39 @@ const getOneMember = (req, res) => {
   }
 };
 
-module.exports = { getAllMembers, getOneMember };
+const createNewMember = (req, res) => {
+  const { body } = req;
+  if (
+    !body.name ||
+    !body.gender ||
+    !body.dateOfBirth ||
+    !body.email ||
+    !body.password
+  ) {
+    res.status(400).send({
+      status: 'FAILED',
+      data: {
+        error:
+          "One of the following keys is missing or is empty in request body: 'name', 'gender', 'dateOfBirth', 'email', 'password'",
+      },
+    });
+    return;
+  }
+  const newMember = {
+    name: body.name,
+    gender: body.gender,
+    dateOfBirth: body.dateOfBirth,
+    email: body.email,
+    password: body.password,
+  };
+  try {
+    const createdMember = memberService.createNewMember(newMember);
+    res.status(201).send({ status: 'OK', data: createdMember });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: 'FAILED', data: { error: error?.message || error } });
+  }
+};
+
+module.exports = { getAllMembers, getOneMember, createNewMember };
