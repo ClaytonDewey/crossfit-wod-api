4e8a7d176d0f4aa95044339ef9ae750ecfbc165e
ClaytonDewey <clay@dryadmedia.com>
2023-01-12 11:12:31 -0600

    Get single member by id

--- src/database/Member.js
+++ src/database/Member.js
@@ -8,4 +8,19 @@ const getAllMembers = () => {
   }
 };
 
-module.exports = { getAllMembers };
+const getOneMember = (memberId) => {
+  try {
+    const member = DB.members.filter((member) => member.id === memberId);
+    if (!member) {
+      throw {
+        status: 400,
+        message: `Can't find member with the id '${memberId}'`,
+      };
+    }
+    return member;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+module.exports = { getAllMembers, getOneMember };
