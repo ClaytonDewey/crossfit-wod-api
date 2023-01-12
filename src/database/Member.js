04b9b0208d3393b1fd5d1309d6dfa1bae1f2424b
ClaytonDewey <clay@dryadmedia.com>
2023-01-12 14:12:25 -0600

    Add updateMember method

--- src/database/Member.js
+++ src/database/Member.js
@@ -42,4 +42,40 @@ const createNewMember = (newMember) => {
   }
 };
 
-module.exports = { getAllMembers, getOneMember, createNewMember };
+const updateOneMember = (memberId, changes) => {
+  try {
+    const isAlreadyAdded =
+      DB.members.findIndex((member) => member.name === changes.name) > -1;
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Member with the name '${changes.name}' already exists`,
+      };
+    }
+    const indexForUpdate = DB.members.findIndex(
+      (member) => member.id === memberId
+    );
+    if (indexForUpdate === -1) {
+      throw {
+        status: 400,
+        message: `Can't find member with the id '${memberId}`,
+      };
+    }
+    const updatedMember = {
+      ...DB.members[indexForUpdate],
+      ...changes,
+    };
+    DB.workouts[indexForUpdate] = updatedMember;
+    saveToDatabase(DB);
+    return updatedMember;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+module.exports = {
+  getAllMembers,
+  getOneMember,
+  createNewMember,
+  updateOneMember,
+};
