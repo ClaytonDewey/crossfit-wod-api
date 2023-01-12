c98c73b150601c2e2e229d25c7fda05af9c57f02
ClaytonDewey <clay@dryadmedia.com>
2023-01-12 14:55:26 -0600

    Add delete member route

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -6,25 +6,19 @@ const memberController = require('../../controllers/memberController');
 const router = express.Router();
 
 router.get('/', workoutController.getAllWorkouts);
-
 router.get('/records', recordController.getAllRecords);
-
 router.get('/members', memberController.getAllMembers);
-
 router.get('/:workoutId', workoutController.getOneWorkout);
-
 router.get('/:workoutId/records', recordController.getRecordForWorkout);
-
 router.get('/members/:memberId', memberController.getOneMember);
 
 router.post('/', workoutController.createNewWorkout);
-
 router.post('/member', memberController.createNewMember);
 
 router.patch('/:workoutId', workoutController.updateOneWorkout);
-
 router.patch('/members/:memberId', memberController.updateOneMember);
 
 router.delete('/:workoutId', workoutController.deleteOneWorkout);
+router.delete('/:member', memberController.deleteOneMember);
 
 module.exports = router;
